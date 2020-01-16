db406e32c105502b5d8a9cd3c1b299bb653522e4
usmanajibola <usmanajibolaabass@gmail.com>
2020-01-16 15:31:00 +0100

    lab5

--- myscrumy/myscrumy/settings.py
+++ myscrumy/myscrumy/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'myscrumy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'first_db',
+        'USER': 'usman',
+	'PASSWORD': 'usman',
+	'HOST': 'localhost',
     }
 }
 
