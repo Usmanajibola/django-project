c2ff151f6a285c0de02dca1f135ba3d589589968
usmanajibola <usmanajibolaabass@gmail.com>
2020-01-20 15:58:45 +0100

    lab11

--- myscrumy/usmanajibolaabassscrumy/views.py
+++ myscrumy/usmanajibolaabassscrumy/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 from usmanajibolaabassscrumy.models import GoalStatus, ScrumyGoals, ScrumyHistory
 
 # Create your views here.
-goal = ScrumyGoals.objects.filter(goal_name="Learn Django")
-def get_grading_parameters(request):
-    return HttpResponse(goal)
+#goal = ScrumyGoals.objects.filter(goal_name="Learn Django")
+#def get_grading_parameters(request):
+#    return HttpResponse(goal)
 
 def move_goal(request, goal_id):
     goalname = Scrumygoals.objects.get(goal_id = '%s' % goal_id)
-    return HttpResponse(goalname)
+    return HttpResponse(goalname.goal_name)
