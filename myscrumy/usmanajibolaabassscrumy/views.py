45bf68bf0d2a8ab5620a52132990d6ad35dd06c9
usmanajibola <usmanajibolaabass@gmail.com>
2020-01-20 05:05:35 +0100

    lab10

--- myscrumy/usmanajibolaabassscrumy/views.py
+++ myscrumy/usmanajibolaabassscrumy/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from usmanajibolaabassscrumy.models import GoalStatus, ScrumyGoals, ScrumyHistory
 
 # Create your views here.
+goal = ScrumyGoals.objects.filter(goal_name="Learn Django")
 def get_grading_parameters(request):
-    return HttpResponse("This is a Scrum Application")
+    return HttpResponse(goal)
