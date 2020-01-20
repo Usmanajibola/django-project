f81b861e603d335d03a6bbb0a95944f7a73280c6
usmanajibola <usmanajibolaabass@gmail.com>
2020-01-20 15:16:59 +0100

    lab11

--- myscrumy/usmanajibolaabassscrumy/urls.py
+++ myscrumy/usmanajibolaabassscrumy/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns=[
-    path('',views.get_grading_parameters)
+    path('',views.get_grading_parameters),
+    path('movegoal/<int:goal_id>', views.move_goal),
 ]
--- myscrumy/usmanajibolaabassscrumy/views.py
+++ myscrumy/usmanajibolaabassscrumy/views.py
@@ -6,3 +6,7 @@ from usmanajibolaabassscrumy.models import GoalStatus, ScrumyGoals, ScrumyHistor
 goal = ScrumyGoals.objects.filter(goal_name="Learn Django")
 def get_grading_parameters(request):
     return HttpResponse(goal)
+
+def move_goal(request, goal_id):
+    goalname = Scrumygoals.objects.get(goal_id = '%s' % goal_id)
+    return HttpResponse(goalname)
